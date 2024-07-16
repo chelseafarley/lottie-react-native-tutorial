ab3df20449ce6cd2ee36dbb66bca8fab6e2cbe6d
Chelsea <chelsea@tripwiretech.com>
2024-07-16 22:09:55 +1200

    Tutorial

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import LottieView from "lottie-react-native";
+import { useRef } from "react";
+
+// npx expo install lottie-react-native
 
 export default function App() {
+  const animation = useRef(null);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LottieView
+        autoPlay={true}
+        ref={animation}
+        style={styles.animation}
+        source={require("./assets/My_Notification.json")}
+      />
+      <Button
+        title="Restart Animation"
+        onPress={() => {
+          animation.current?.reset();
+          animation.current?.play();
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  animation: {
+    width: 200,
+    height: 200,
   },
 });
